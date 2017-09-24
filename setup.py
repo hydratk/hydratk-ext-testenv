68440e420ba7ac909828795ddca65eb9f3540b06
Petr Rašek <bowman@hydratk.org>
2017-09-24 12:29:01 +0200

    Modified - FreeBSD platform supported.

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ st_setup(
     entry_points=entry_points,
     keywords='hydratk,testing,test environment,web server,database',
     requires_python='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*',
-    platforms='Linux'
+    platforms='Linux,FreeBSD'
 )
 
 task.run_post_install(argv, config)
